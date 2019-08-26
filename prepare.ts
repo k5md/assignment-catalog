23e27dcad8664c73d54d543f66d127290139739e
k5md <serg.dfc@yandex.ru>
2019-08-26 18:55:22 +0300

    fix incorrect values generated for color

--- prepare.ts
+++ prepare.ts
@@ -6,6 +6,16 @@ const { format } = require('date-fns');
 // https://www.reddit.com/r/typescript/comments/amh8cu/dynamically_build_typescript_string_literal_type/eflx6at/
 function asLiterals<T extends string>(arr: T[]): T[] { return arr; }
 
+const getRandomColor = (): string => {
+  const rgb = Array(3).fill(null).map(() => {
+    const component = _.random(0, 255).toString(16);
+    const padded = _.pad(component, 2, '0');
+    return padded;
+  });
+  const hexString = `#${rgb.join('')}`;
+  return hexString;
+};
+
 const datasetSize = 1000;
 const productTypesAmount = 100; 
 
@@ -30,7 +40,7 @@ class Product implements IProduct {
   id: number =  _.uniqueId();
   name: string = faker.commerce.productName();
   type: typeof productTypesLiterals[number] = faker.random.arrayElement(productTypes);
-  color: string = `#${Math.floor(Math.random() * 0xFFFFFF)}`;
+  color: string = getRandomColor();
   size: typeof productSizesLiterals[number] = faker.random.arrayElement(productSizes);
   inStock: boolean = faker.random.boolean();
   dateReceipt: string = format(new Date(faker.date.past()), 'yyyy-MM-dd');

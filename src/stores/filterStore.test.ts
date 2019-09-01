ba3d6504d616c1f59565bb474874db7242b520c3
k5md <serg.dfc@yandex.ru>
2019-09-02 02:15:39 +0300

    fix filterStore test

--- src/stores/filterStore.test.ts
+++ src/stores/filterStore.test.ts
@@ -1,68 +1,17 @@
 import _ from 'lodash';
-import moment from 'moment';
 import { FilterStore } from './FilterStore';
+import { StockFilter } from './Filter';
 
-const filterDefaults = {
-  type: 'Верхняя одежда',
-  color: '#37be70',
-  size: 'S',
-  inStock: false,
-  dateRange: [
-    moment().subtract(1, 'months').format('YYYY-MM-DD'),
-    moment().format('YYYY-MM-DD')
-  ],
-};
-
-const products = [
-  {
-    "id": 1,
-    "name":"Маленький Хлопковый Берет",
-    "type":"Штанишки",
-    "color":"#a0764b",
-    "size": 'M',
-    "inStock":false,
-    "dateReceipt":"2018-12-25"
-  },
-  {
-    "id": 2,
-    "name":"Невероятный Пластиковый Стул",
-    "type":"Белье",
-    "color":"#4c528a",
-    "size": 'S',
-    "inStock":true,
-    "dateReceipt":"2018-09-14"
-  },
-];
-
-describe('FilterModel', () => {
-  const filters = {
-    'inStock': { valuesToSet: products[1].inStock, expectedLength: 1 },
-    'size': { valuesToSet: products[0].size, expectedLength: 1 },
-    'type': { valuesToSet: products[1].type, expectedLength: 1 },
-    'dateRange': { valuesToSet: [ '2018-09-13', '2018-12-26' ], expectedLength: 2 },
-  };
+describe('filterStoreModel', () => {
+  const _filters = [ StockFilter.create() ];
+  const store = FilterStore.create({
+    _filters,
+  });
+  it('creates model', () => {
+    expect(store).toBeTruthy();
+  })
   
-  Object.keys(filters).forEach((filterName) => {
-    const filterStore = FilterStore.create();
-
-    const { valuesToSet, expectedLength } = filters[filterName];
-    test(`${filterName} has get method that return correct default values`, () => {     
-      expect(filterStore[filterName]).toEqual(filterDefaults[filterName])
-    });
-
-    test(`${filterName} has set method to set property`, () => {
-      // filterName => setFilterName
-      const action = `set${_.upperFirst(filterName)}`;
-      filterStore[action](valuesToSet)
-      expect(filterStore[filterName]).toEqual(valuesToSet)
-    });
-
-    test(`${filterName} filters array on related property`, () => {
-       // filterName => setFilterNameEnabled
-      const toggle = `set${_.upperFirst(filterName)}Enabled`;
-      filterStore[toggle]();
-      const filtered = filterStore.filters(products);
-      expect(filtered.length).toEqual(expectedLength);
-    });
+  it('returns a map with filterType keys', () => {
+    expect(store.filters).toHaveProperty('stock');
   });
 });
